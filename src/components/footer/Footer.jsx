0c466adf011d99e11728c95461d808cae39ed3fe
Tito <matiasahrensdorf@gmail.com>
2022-04-06 22:21:29 -0400

    footer/header styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { AiOutlineLinkedin } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <section id='footer'>Footer</section>
+        <footer>
+            <a href="#" className='footer_logo'>Matias</a>
+
+            <ul className='permalinks'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className='footer_socials'>
+                <a href="https://facebook.com"><FaFacebookF /></a>
+                <a href="https://instagram.com"><FiInstagram /></a>
+                <a href="https://linkedin.com"><AiOutlineLinkedin /></a>
+            </div>
+
+            <div className="footer_copyright">
+                <small>&copy; My Portfolio. All rights reserved.</small>
+            </div>
+        </footer>
     )
 }
 
