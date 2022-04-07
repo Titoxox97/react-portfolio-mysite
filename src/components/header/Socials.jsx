8b82c4d275af1115c7d45eb985ec65e5fc93dfdb
Tito <matiasahrensdorf@gmail.com>
2022-04-06 22:28:56 -0400

    header styling

--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -5,7 +5,7 @@ import { GrInstagram } from 'react-icons/gr'
 
 const Socials = () => {
     return (
-        <div className="header__socials">
+        <div className="header_socials">
             <a href="http://linkedin.com" target="_blank"><BsLinkedin /></a>
             <a href="http://github.com" target="_blank"><FaGithubSquare /></a>
             <a href="http://instagram.com" target="_blank"><GrInstagram /></a>
